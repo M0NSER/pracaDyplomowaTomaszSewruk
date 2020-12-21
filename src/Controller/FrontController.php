a9467f14952bb61f05f543e645d0f510ad98cbc2
tsewruk <tsewruk@doz.pl>
2020-12-21 13:06:58 +0100

    Dodano tesowe templates'y

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -13,10 +13,33 @@ class FrontController extends AbstractController
      */
     public function index(): Response
     {
-//        return $this->render('front/index.html.twig', [
-        return $this->render('base.html.twig', [
+        return $this->render('nieposegregowane/options.html.twig', [
             'controller_name' => 'FrontController',
         ]);
 
+    }
+
+    /**
+     * @Route("/options", name="options")
+     */
+    public function options(): Response
+    {
+        return $this->render('nieposegregowane/options.html.twig');
+    }
+
+    /**
+     * @Route("/choosen-option", name="choosen-option")
+     */
+    public function chooseOption(): Response
+    {
+        return $this->render('nieposegregowane/choosen_option.html.twig');
+    }
+
+    /**
+     * @Route("/create-competition", name="create-competition")
+     */
+    public function createCompetition(): Response
+    {
+        return $this->render('nieposegregowane/create-competition.html.twig');
     }
 }
